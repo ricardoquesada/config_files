5ee79288b0b1b1504bda6bfd86e369cf3c1d305e
Ricardo Quesada <ricardoquesada@gmail.com>
2020-08-13 13:47:09 -0700

    deb: more tools

--- debs/install.sh
+++ debs/install.sh
@@ -1,3 +1,3 @@
 sudo apt install clang-format clang gdb gcc g++ llvm clangd golang rustc nasm lldb
 sudo apt install git mercurial meson autoconf automake ninja-build make
-sudo apt install strace neovim tmux wajig wget wireshark mc meld
+sudo apt install strace neovim tmux wajig wget wireshark mc meld ripgrep silversearcher-ag
