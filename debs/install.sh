e0cf5bfb2a6abda548f01b2b4ecba82a31fbc053
Ricardo Quesada <ricardoquesada@gmail.com>
2020-08-13 09:27:52 -0700

    deb: more packages to install

--- debs/install.sh
+++ debs/install.sh
@@ -1,1 +1,3 @@
-sudo apt install strace clang-format neovim clang gdb gcc g++ llvm clangd golang git mercurial meson autoconf
+sudo apt install clang-format clang gdb gcc g++ llvm clangd golang rustc nasm lldb
+sudo apt install git mercurial meson autoconf automake ninja-build make
+sudo apt install strace neovim tmux wajig wget wireshark mc meld
