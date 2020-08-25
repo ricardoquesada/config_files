6be143e3c31a596c0fc3f4d2bde70932089fb0b7
Ricardo Quesada <ricardoquesada@gmail.com>
2020-08-25 14:56:34 -0700

    add nvim related files

--- debs/install.sh
+++ debs/install.sh
@@ -1,3 +1,8 @@
 sudo apt install clang-format clang gdb gcc g++ llvm clangd golang rustc nasm lldb
 sudo apt install git mercurial meson autoconf automake ninja-build make
 sudo apt install strace neovim tmux wajig wget wireshark mc meld ripgrep silversearcher-ag
+
+# nvim related
+sudo apt install exuberant-ctags python-dev python3-dev fonts-powerline ripgrep curl python3-pip xclip clangd tmux fuse
+$ pip3 install --user --upgrade pynvim
+$ pip3 install --user --upgrade msgpack
