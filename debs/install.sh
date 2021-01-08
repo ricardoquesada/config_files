7e7cf44e7cc39f7abb33a053da550dec64f76b6c
Ricardo Quesada <ricardoquesada@gmail.com>
2021-01-08 13:28:25 -0800

    Add missing debs: CADs, libs, etc.

--- debs/install.sh
+++ debs/install.sh
@@ -1,8 +1,21 @@
+# compilers, terminal, etc
 sudo apt install clang-format clang gdb gcc g++ llvm clangd golang rustc nasm lldb
 sudo apt install git mercurial meson autoconf automake ninja-build make
 sudo apt install strace neovim tmux wajig wget wireshark mc meld ripgrep silversearcher-ag
 
 # nvim related
-sudo apt install exuberant-ctags python-dev python3-dev fonts-powerline ripgrep curl python3-pip xclip clangd tmux fuse
-$ pip3 install --user --upgrade pynvim
-$ pip3 install --user --upgrade msgpack
+sudo apt install exuberant-ctags python-dev python3-dev fonts-powerline ripgrep curl python3-pip xclip fuse
+pip3 install --user --upgrade pynvim
+pip3 install --user --upgrade msgpack
+
+# bluepad32 related
+sudo apt install libusb-1.0-0-dev
+
+# CADs
+sudo apt install freecad blender kicad kicad-packages3d
+
+# Games
+sudo apt install steam
+
+# Vice
+sudo apt install dos2unix xa65 libsdl2-dev libsdl2-image-dev libpng-dev
