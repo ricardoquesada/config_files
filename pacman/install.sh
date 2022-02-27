a443acca620d7483dd88c35736aa7cbb97375ff9
Ricardo Quesada <ricardoquesada@gmail.com>
2022-02-27 06:46:07 -0800

    pacman: add wireshark

--- pacman/install.sh
+++ pacman/install.sh
@@ -29,5 +29,5 @@ sudo pacman -Syu evtest evemu minicom powerline
 sudo pacman -Syu arj lzip pbzip2 bzip2 p7zip tar file lzip
 
 # Utils
-sudo pacman -Syu binwalk hexer cpio
+sudo pacman -Syu binwalk hexer cpio wireshark-qt
 
