9733e952e2970b76d4ab38c5673861903c59ecbf
Ricardo Quesada <ricardoquesada@gmail.com>
2022-11-13 08:03:04 -0800

    clean up utils to install

--- debs/install.sh
+++ debs/install.sh
@@ -1,14 +1,14 @@
 # compilers, terminal, etc
 sudo apt install clang-format clang gdb gcc g++ llvm clangd golang rustc nasm lldb
 sudo apt install git mercurial meson autoconf automake ninja-build make
-sudo apt install strace neovim tmux wajig wget mc ripgrep silversearcher-ag
+sudo apt install strace neovim tmux wajig wget curl mc ripgrep silversearcher-ag
 sudo apt install htop
 
 # GUI tools
 sudo apt install wireshark meld 
 
 # nvim related
-sudo apt install exuberant-ctags python-dev python3-dev fonts-powerline ripgrep curl python3-pip xclip fuse
+sudo apt install exuberant-ctags python3-dev fonts-powerline python3-pip xclip fuse
 pip3 install --user --upgrade pynvim
 pip3 install --user --upgrade msgpack
 
