4e34c768f1628b5d22e2f05cd43ab2d49022b9e7
Ricardo Quesada <ricardoquesada@gmail.com>
2026-01-24 07:08:45 -0800

    remove rust. will be install from rustup

--- setup_mac/setup.sh
+++ setup_mac/setup.sh
@@ -7,7 +7,7 @@ brew install fzf
 brew install fd
 
 # compilers and tools
-brew install cmake clang-format ninja dfu-util meson rust flutter cocoapods
+brew install cmake clang-format ninja dfu-util meson flutter cocoapods
 
 brew install git tmux nvim luarocks lazygit tree-sitter tree-sitter-cli
 brew install pkgconfig libusb
